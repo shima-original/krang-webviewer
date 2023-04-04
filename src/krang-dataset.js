63eeb2ac8a0317f311cb3b44242388e40fd7c2ba
Olga Mkrtchyan <olia.mst@gmail.com>
2023-04-05 01:46:23 +0300

    wip

--- src/krang-dataset.js
+++ src/krang-dataset.js
@@ -1,47 +1,62 @@
-function traverseXmlDoc(rootTag, maxDeepness, tagCallback, path=""){
-	if(!rootTag.tagName) return;
-	var myPath = path + "." + rootTag.tagName;
-	tagCallback(myPath, rootTag);
+function traverseXmlDoc(rootTag, maxDeepness, tagCallback, path = "") {
+  if (!rootTag.tagName) return;
+  var myPath = path + "." + rootTag.tagName;
+  tagCallback(myPath, rootTag);
 
-	if(maxDeepness > 0) {
-		rootTag.childNodes.forEach((child) => {traverseXmlDoc(child, maxDeepness-1, tagCallback, myPath)})
-	}
+  if (maxDeepness > 0) {
+    rootTag.childNodes.forEach((child) => {
+      traverseXmlDoc(child, maxDeepness - 1, tagCallback, myPath);
+    });
+  }
 }
 
-async function krang_listPreviews(){
-	try {
-		let response = await fetch("https://storage.yandexcloud.net/krang-dataset?list-type=2&prefix=thumbnails/")
-		let text = await response.text()
-		console.log("response", response)
-		let parser = new DOMParser()
-		let doc = await parser.parseFromString(text, "text/xml")
+async function loadCollection() {
+  try {
+    const parser = new DOMParser();
 
-		const filenames = []
-		const previewPattern = /[0-9a-f]{12}-preview.jpg/
+    const URLprefix = "https://krang-dataset.website.yandexcloud.net/";
 
-		traverseXmlDoc(doc.documentElement, 5, (path, tag)=>{
-			if(path == ".ListBucketResult.Contents.Key"){
-				console.log("filename", tag.textContent)
-				if(previewPattern.test(tag.textContent)){
-					filenames.push(tag.textContent);
-				}
-			}
-		});
-		console.log("filenames", filenames);
+    const response = await fetch(
+      "https://storage.yandexcloud.net/krang-dataset?list-type=2"
+    );
 
-		const prefix = "https://krang-dataset.website.yandexcloud.net/"
-		const list = filenames.map(filename => {
-			return {
-				url: prefix + filename,
-				id: filename.slice(11,23)
-				}
-		})
-		console.debug(list);
+    const doc = parser.parseFromString(await response.text(), "text/xml");
 
-		return list
+    let entries = {};
 
-	} catch(e) {
-		console.error("Error while loading list of previews", e)
-		return null;
-	}
+    const patterns = {
+      fragment: /[0-9a-f]{12}-cut/,
+      preview: /[0-9a-f]{12}-preview.jpg/,
+    };
+
+    traverseXmlDoc(doc.documentElement, 5, (path, tag) => {
+      if (path === ".ListBucketResult.Contents.Key") {
+        const isFragment = patterns.fragment.test(tag.textContent),
+          isPreview = patterns.preview.test(tag.textContent);
+
+        const id = tag.textContent.split("/");
+
+        console.log(id);
+
+        entries = {
+          ...entries,
+
+          [id]: {
+            ...(entries[id] ?? {}),
+
+            id,
+            fragmentURL: isFragment ? URLprefix + tag.textContent : null,
+            previewURL: isPreview ? URLprefix + tag.textContent : null,
+          },
+        };
+      }
+    });
+
+    console.log(entries);
+
+    return entries;
+  } catch (error) {
+    console.error("Error while loading data", error);
+    return null;
+  }
 }
